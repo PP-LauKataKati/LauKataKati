8ada977a23e2495f39070e9e691eda3147985133
kowaliklukasz <lukasz.m.kowalik@gmail.com>
2020-04-26 17:44:14 +0200

    Shifting pawns

--- main.py
+++ main.py
@@ -18,35 +18,57 @@ board = [None] * 21
 board[9] = 'X'
 board[11] = 'X'
 
-black_pos = [[85, 55], [351, 55], [618, 55], [177, 155], [351, 155], [529, 155], [262, 255], [351, 255], [440, 255]]
-white_pos = [[85, 655], [351, 655], [618, 655], [177, 555], [351, 555], [529, 555], [262, 455], [351, 455], [440, 455]]
-all_pos = [[85, 55], [351, 55], [618, 55], [177, 155], [351, 155], [529, 155], [262, 255], [351, 255], [440, 255],
-           [85, 655], [351, 655], [618, 655], [177, 555], [351, 555], [529, 555], [262, 455], [351, 455], [440, 455], [351,  355]]
+black_pos = [[110, 70], [368, 70], [630, 70], [190, 170], [368, 170], [545, 170], [279, 270], [368, 270], [457, 270]]
+white_pos = [[110, 670], [368, 670], [630, 670], [190, 570], [368, 570], [545, 570], [279, 470], [368, 470], [457, 470]]
+all_pos = [[110, 70], [368, 70], [630, 70], [190, 170], [368, 170], [545, 170], [279, 270], [368, 270], [457, 270],
+           [110, 670], [368, 670], [630, 670], [190, 570], [368, 570], [545, 570], [279, 470], [368, 470], [457, 470]]
 
 black_pawns = []
 white_pawns = []
 
+done = False
+
 for pos in black_pos:
-    pioneczek = black_pawn.BlackPawn(pos[0], pos[1])
-    black_pawns.append(pioneczek)
+    pawn = black_pawn.BlackPawn(pos[0], pos[1])
+    black_pawns.append(pawn)
 
 for pos in white_pos:
-    pioneczek = white_pawn.WhitePawn(pos[0], pos[1])
-    white_pawns.append(pioneczek)
+    pawn = white_pawn.WhitePawn(pos[0], pos[1])
+    white_pawns.append(pawn)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN and not done:
+            done = True
             xMouse = event.pos[0]
             yMouse = event.pos[1]
             print(xMouse, yMouse)
-            for pawn in white_pawns:
-                print(pawn.x, pawn.y)
-                if pawn.rect.collidepoint(xMouse, yMouse):
-                    pawn.x = xMouse
-                    pawn.y = yMouse
+
+            clicked_white = [p for p in white_pawns if p.rect.collidepoint(xMouse, yMouse)]
+            clicked_black = [p for p in black_pawns if p.rect.collidepoint(xMouse, yMouse)]
+
+            if len(clicked_white) == 1:
+                clicked_pawn = clicked_white[0]
+
+            elif len(clicked_black) == 1:
+                clicked_pawn = clicked_black[0]
+
+            # for pawn in white_pawns:
+            #     print(pawn.x, pawn.y)
+            #     if pawn.rect.collidepoint(xMouse, yMouse):
+            #         pawn.x = xMouse
+            #         pawn.y = yMouse
+            #         pawn.rect.center = (pawn.x, pawn.y)
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and done:
+            done = False
+            xMouse2 = event.pos[0]
+            yMouse2 = event.pos[1]
+            clicked_pawn.x = xMouse2
+            clicked_pawn.y = yMouse2
+            clicked_pawn.rect.center = (clicked_pawn.x, clicked_pawn.y)
 
     screen.blit(background, (0, 0))
 
