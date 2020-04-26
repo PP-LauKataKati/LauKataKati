9083ab21ea23d3d8a9b8b31377db987b6d61879a
rleszkiewicz <radekleszkiewicz@gmail.com>
2020-04-26 16:38:14 +0200

    added ignore file and venv

--- white_pawn.py
+++ white_pawn.py
@@ -5,6 +5,6 @@ class WhitePawn:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load('assets/white_.png')
+        self.image = pygame.image.load('assets/white.png')
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
